6b9b64aa7f305262a16c01f3a5adcf2240c2239a
myml <wurongjie1@gmail.com>
2024-06-10 17:46:32 +0800

    feat: remove debug code

--- pip.go
+++ pip.go
@@ -37,7 +37,7 @@ func pipMirror(ctx context.Context, addr string, bucket, prefix, remote string) 
 				return nil
 			}
 			// 小于1M时直接返回
-			if resp.ContentLength < 1024*1024 && false {
+			if resp.ContentLength < 1024*1024 {
 				copyHander(w, resp)
 				_, err = io.Copy(w, resp.Body)
 				if err != nil {
